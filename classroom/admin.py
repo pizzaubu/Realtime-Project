f5fbc9f191520008027a358d87b86589cf507540
pizzaubu <pongpanot.wa.63@ubu.ac.th>
2023-10-06 19:37:23 +0700

    fix admin.py

--- classroom/admin.py
+++ classroom/admin.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
-
 from .models import *
 
-# Register your models here.
-# admin/1234
-admin.site.register(Course)
-admin.site.register(Student)
+# Admin class for Student
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email']
+
+# Admin class for Teacher
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email']
+
+# Admin class for Course
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['id', 'subject_name', 'teach_date', 'start_time', 'end_time']
+
+# Admin class for CourseStudent
+class CourseStudentAdmin(admin.ModelAdmin):
+    list_display = ['course', 'student']
+
+# Admin class for CourseTeacher
+class CourseTeacherAdmin(admin.ModelAdmin):
+    list_display = ['course', 'teacher']
+
+# Admin class for Assignment
+class AssignmentAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'due_date', 'course']
+
+# Register the models and their corresponding admin classes
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(CourseStudent, CourseStudentAdmin)
+admin.site.register(CourseTeacher, CourseTeacherAdmin)
+admin.site.register(Assignment, AssignmentAdmin)
